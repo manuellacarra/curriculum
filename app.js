f87e50737c204ed368c326bd6997beb645705e7d
manuellacarra <66756616+manuellacarra@users.noreply.github.com>
2020-10-07 13:17:03 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+$("#javascript").hover(function(){ 
+    $("#slide1").slideDown(); 
+});
+
+$("#javascript").mouseleave(function(){ 
+    $("#slide1").slideUp(); 
+});
+
+$("#css").hover(function(){ 
+    $("#slide2").slideDown(); 
+});
+
+$("#css").mouseleave(function(){ 
+    $("#slide2").slideUp(); 
+});
+
